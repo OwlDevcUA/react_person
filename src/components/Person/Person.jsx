3828a1b16c23b23104ad26543983c84a455b8bbd
Owk <kkoctuk2005@gmail.com>
2025-05-04 17:30:47 +0300

    task solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,17 @@
-// export const Person = ({ person }) => ();
+export const Person = ({ person }) => (
+  <section className="Person">
+    <h2 className="Person__name">My name is {person.name}</h2>
+
+    {'age' in person && <p className="Person__age">I am {person.age}</p>}
+
+    {person.isMarried && person.sex === 'm' && (
+      <p className="Person__partner">Natasha is my wife</p>
+    )}
+
+    {person.isMarried && person.sex === 'f' && (
+      <p className="Person__partner">Maksym is my husband</p>
+    )}
+
+    {!person.isMarried && <p className="Person__partner">I am not married</p>}
+  </section>
+);
