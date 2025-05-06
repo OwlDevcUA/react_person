41a342ba875f3515b79700d516ef49a4448fef1c
Owk <kkoctuk2005@gmail.com>
2025-05-06 22:55:57 +0300

    task solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,9 +1,10 @@
 export const Person = ({ person }) => {
   const { name, age, sex, isMarried, partnerName } = person;
 
-  const isMarriedMale = isMarried && sex === 'm';
-  const isMarriedFemale = isMarried && sex === 'f';
-  const isSingle = !isMarried;
+  const partnerRole = sex === 'm' ? 'wife' : 'husband';
+  const partnerText = isMarried
+    ? `${partnerName} is my ${partnerRole}`
+    : 'I am not married';
 
   return (
     <section className="Person">
@@ -11,15 +12,7 @@ export const Person = ({ person }) => {
 
       {age && <p className="Person__age">I am {age}</p>}
 
-      {isMarriedMale && (
-        <p className="Person__partner">{partnerName} is my wife</p>
-      )}
-
-      {isMarriedFemale && (
-        <p className="Person__partner">{partnerName} is my husband</p>
-      )}
-
-      {isSingle && <p className="Person__partner">I am not married</p>}
+      <p className="Person__partner">{partnerText}</p>
     </section>
   );
 };
